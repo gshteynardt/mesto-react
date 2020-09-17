a98262f29c939ea5411d9dababfe91c89b172baa
German Shteynardt <gvshteynardt@gmail.com>
2020-09-17 14:19:13 +0300

    feat: компонент рендеринга карточек

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import Header from '../Header/Header';
 import Main from '../Main';
 import Footer from '../Footer/Footer';
